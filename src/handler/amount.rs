384f16cb77ed6075527346e1f2cfee49668dd92c
Jvm_Laptop <jvmlaptop@mail.com>
2026-09-21 19:39:06 +0700

    ExchangerHandler: is done, needed to replaced old-fn-system handlers

--- src/handler/amount.rs
+++ src/handler/amount.rs
@@ -1,96 +1,80 @@
 use crate::model::amount::{AnyExchange, ExchangeRate};
 use crate::model::error::AppError;
-use std::collections::HashMap;
-use std::io::stdin;
 use crate::traits::amount::Exchange;
+use crate::traits::handler::StrHandler;
+use std::collections::HashMap;
 
-pub fn exchange_rate_handler() -> Result<(), AppError> {
-    let mut array: Vec<AnyExchange> = Vec::with_capacity(10);
+pub struct ExchangeHandler {
+    array: Vec<AnyExchange>,
+}
 
-    loop {
-        println!("Enter exchange rate OR Q for exit: ");
-        println!("#1 Schema: <amount_name> <amount_name> <rate value> <date> ");
-        println!("#2 Schema: <bank name> <fee value> <rate sell> <rate buy> #1 Schema");
-        println!("#3 Schema: <person name> <rate sell> <rate buy> #1 Schema");
+impl ExchangeHandler {
+    pub fn new() -> ExchangeHandler {
+        ExchangeHandler { array: Vec::new() }
+    }
 
-        let mut input = String::new();
+    pub fn unpack(&self) -> Vec<ExchangeRate> {
+        self.array
+            .iter()
+            .map(|rate| match rate {
+                AnyExchange::Rate(e) => e.clone(),
+                AnyExchange::Bank(e) => e.exchange_rate(),
+                AnyExchange::Person(e) => e.exchange_rate(),
+            })
+            .collect::<Vec<ExchangeRate>>()
+    }
 
-        match stdin().read_line(&mut input) {
-            Ok(_) => {}
-            Err(_) => {
-                eprintln!("Error reading input");
-                continue;
-            }
-        };
-        let input = input.trim();
+    fn get_big_rate(&self) -> Result<ExchangeRate, AppError> {
+        let rates = self.unpack();
+        let main_amount = Self::get_main_amount(&rates); // dollar
+        let mut amount: Vec<ExchangeRate> = rates
+            .into_iter()
+            .filter(|x| x.from == main_amount)
+            .collect::<Vec<ExchangeRate>>();
+        amount.sort_by(|x, y| x.rate.partial_cmp(&y.rate).unwrap());
 
-        if input == "Q" {
-            break;
+        match amount.last() {
+            Some(last) => Ok(last.clone()),
+            None => Err(AppError::InternalError("The exchange rate is empty".to_string())),
+        }
+    }
+
+    fn get_main_amount(rates: &[ExchangeRate]) -> String {
+        let froms = rates
+            .iter()
+            .map(|r| r.from.to_string())
+            .collect::<Vec<String>>();
+        let mut map = HashMap::new();
+        for form in &froms {
+            map.insert(form, 1);
         }
 
+        for form in &froms {
+            let v = map[&form];
+            map.insert(form, v + 1);
+        }
+
+        let max_pair = map.iter().max_by_key(|&(_, value)| value);
+
+        match max_pair {
+            Some((key, _)) => key.to_string(),
+            None => panic!("Map is empty"),
+        }
+    }
+}
+
+impl StrHandler for ExchangeHandler {
+    fn handle(&mut self, input: &str) -> Result<(), AppError> {
+        let input = input.trim();
         let rate = match AnyExchange::make_from_string(input) {
             Ok(r) => r,
             Err(e) => {
-                eprintln!("Error input, {}", e);
-                continue;
+                return Err(e);
             }
         };
-
         println!("{:?}", rate);
         println!("{:?}", rate);
-        array.push(rate);
-
-    }
-
-    println!("{}", get_big_rate(&unpack(&array)));
-    Ok(())
-}
-
-pub fn unpack(rates: &[AnyExchange]) -> Vec<ExchangeRate> {
-    rates
-        .iter()
-        .map(|rate| match rate {
-            AnyExchange::Rate(e) => e.clone(),
-            AnyExchange::Bank(e) => e.exchange_rate(),
-            AnyExchange::Person(e) => e.exchange_rate()
-        })
-        .collect::<Vec<ExchangeRate>>()
-}
-
-pub fn get_big_rate(rates: &[ExchangeRate]) -> &ExchangeRate {
-    let main_amount = get_main_amount(rates); // dollar
-    let mut amount: Vec<&ExchangeRate> = rates
-        .iter()
-        .filter(|x| x.from == main_amount)
-        .collect::<Vec<&ExchangeRate>>();
-    amount.sort_by(|x, y| x.rate.partial_cmp(&y.rate).unwrap());
-    match amount.last() {
-        None => {
-            panic!("No exchange rate available");
-        }
-        Some(a) => a,
-    }
-}
-
-fn get_main_amount(rates: &[ExchangeRate]) -> String {
-    let froms = rates
-        .iter()
-        .map(|r| r.from.to_string())
-        .collect::<Vec<String>>();
-    let mut map = HashMap::new();
-    for form in &froms {
-        map.insert(form, 1);
-    }
-
-    for form in &froms {
-        let v = map[&form];
-        map.insert(form, v + 1);
-    }
-
-    let max_pair = map.iter().max_by_key(|&(_, value)| value);
-
-    match max_pair {
-        Some((key, _)) => key.to_string(),
-        None => panic!("Map is empty"),
+        self.array.push(rate);
+        Ok(())
     }
 }
