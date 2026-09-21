f6e1bb94efae3350c84a9983f629ccf04a1817e7
Jvm_Laptop <jvmlaptop@mail.com>
2026-09-21 18:20:25 +0700

    Menu struct is gotten more methods

--- src/handler/menu.rs
+++ src/handler/menu.rs
@@ -1,21 +1,45 @@
+use crate::model::error::AppError;
 use std::io::stdin;
+use std::process::Command;
 
-pub struct Menu {
-}
+pub struct Menu {}
 
-pub fn menu() {
-    println!("Menu");
-    println!("1. Ввод обычного курса");
-    println!("2. Ввод банковского курса");
-    println!("3. Ввод курса от какого-то человека");
+impl Menu {
+    pub fn new() -> Self {
+        Self {}
+    }
 
-    let mut key: String = String::new();
+    pub fn show_main_menu(&self) {
+        self.clear();
+        println!("===== Menu =====");
+        println!("1. Ввод курсов");
+        println!("2. Считать файл");
+        println!("3. Сохранить в файл");
+        println!("4. Выход");
+    }
 
-    match stdin().read_line(&mut key) {
-        Ok(_) => {
+    pub fn show_rates(&self) {
+        self.clear();
+        println!("1. Ввести обычный курс");
+        println!("2. Ввести банковский курс");
+        println!("3. Ввести курс человека");
+        println!("4. Назад");
+    }
+
+    pub fn clear(&self) {
+        if cfg!(target_os = "windows") {
+            Command::new("cls").status().ok();
+        } else {
+            Command::new("clear").status().ok();
         }
-        Err(e) => {
-            println!("Error: {}", e);
+    }
+
+    pub fn get_input(&self) -> Result<String, AppError> {
+        let mut input: String = String::new();
+
+        if let Err(e) = stdin().read_line(&mut input) {
+            return Err(AppError::IoError(e));
         }
+        Ok(input)
     }
 }
