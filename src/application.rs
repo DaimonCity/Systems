8a677ee4d1d5478f5e1ffa258d25c3d821f7fa78
Jvm_Laptop <jvmlaptop@mail.com>
2026-09-21 18:08:57 +0700

    Application struct is created

---
+++ src/application.rs
@@ -0,0 +1,20 @@
+
+pub struct App {
+    
+}
+
+impl App {
+    pub fn new() -> Self {
+        Self{}
+    }
+    
+    pub fn start(&self) {
+        
+    }
+    
+    pub fn app_loop(&self) {
+        loop {
+            
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use crate::handler::amount::exchange_rate_handler;
 mod model;
 mod handler;
 pub mod traits;
+mod application;
 
 fn main() {
     let _ = exchange_rate_handler();
